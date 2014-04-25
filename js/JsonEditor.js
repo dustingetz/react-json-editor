ac80afc28a05c4eb3c209c3e18c34bfc737ea9ac
Dustin Getz <dgetz@wingspan.com>
2014-04-25 16:36:39 -0400

    editor doesn't show an input until you click

--- js/JsonEditor.js
+++ js/JsonEditor.js
@@ -50,7 +50,8 @@ define([
         getInitialState: function () {
             // Stringified value includes "type" - e.g. strings are quoted.
             return {
-                jsValue: JSON.stringify(this.props.cursor.value, undefined, 2)
+                jsValue: JSON.stringify(this.props.cursor.value, undefined, 2),
+                editing: false
             };
         },
 
@@ -67,13 +68,16 @@ define([
                 !this.isValid() ? 'invalid' : null
             ]);
 
+            var leaf = (this.state.editing
+                ? [(<input key="0" value={this.state.jsValue} onChange={this.onChange} style={{background: 'transparent'}}/>),
+                   (<button key="1" onClick={this.commit} disabled={!this.isValid()}>commit</button>)]
+                : [(<code key="2" onClick={this.edit}>{this.state.jsValue}</code>)]);
+
             return (
                 <span className={classes.join(' ')}>
-                    <input value={this.state.jsValue} onChange={this.onChange}
-                    style={{background: 'transparent'}}/>
-                    <button onClick={this.commit} disabled={!this.isValid()}>commit</button>
+                    {leaf}
                 </span>
-                );
+            );
         },
 
         onChange: function (e) {
@@ -82,6 +86,11 @@ define([
 
         commit: function () {
             this.props.cursor.onChange(JSON.parse(this.state.jsValue));
+            this.setState({ editing: false });
+        },
+
+        edit: function () {
+            this.setState({ editing: true });
         },
 
         isValid: function () {
@@ -162,7 +171,6 @@ define([
         return (
             <span key={key}>
                 <code>{key}: </code>
-                <span>{cursor.value}</span>
                 <JsonLeafEditor cursor={cursor} />
             </span>
         );
